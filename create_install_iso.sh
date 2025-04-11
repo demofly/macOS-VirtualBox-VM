9229fa94803d75fa6300f1c6ef1346add7ca2e59
Stanislav O. <1211327+demofly@users.noreply.github.com>
2025-04-11 16:55:36 +0300

    Update create_install_iso.sh to Sequoia release (OS X 15.4)

--- create_install_iso.sh
+++ create_install_iso.sh
@@ -1,24 +1,9 @@
 #!/bin/bash
 set -e
 
-hdiutil attach /Applications/Install\ macOS\ High\ Sierra.app/Contents/SharedSupport/InstallESD.dmg -noverify -mountpoint /Volumes/highsierra
-hdiutil create -o /tmp/HighSierraBase.cdr -size 7316m -layout SPUD -fs HFS+J
-hdiutil attach /tmp/HighSierraBase.cdr.dmg -noverify -mountpoint /Volumes/install_build
-asr restore -source /Applications/Install\ macOS\ High\ Sierra.app/Contents/SharedSupport/BaseSystem.dmg -target /Volumes/install_build -noprompt -noverify -erase
-rm /Volumes/OS\ X\ Base\ System/System/Installation/Packages
-cp -R /Volumes/highsierra/Packages /Volumes/OS\ X\ Base\ System/System/Installation
-hdiutil detach /Volumes/OS\ X\ Base\ System/
-hdiutil detach /Volumes/highsierra/
-mv /tmp/HighSierraBase.cdr.dmg /tmp/BaseSystem.dmg
-
-hdiutil create -o /tmp/HighSierra.cdr -size 8965m -layout SPUD -fs HFS+J
-hdiutil attach /tmp/HighSierra.cdr.dmg -noverify -mountpoint /Volumes/install_build
-asr restore -source /Applications/Install\ macOS\ High\ Sierra.app/Contents/SharedSupport/BaseSystem.dmg -target /Volumes/install_build -noprompt -noverify -erase
-cp /tmp/BaseSystem.dmg /Volumes/OS\ X\ Base\ System
-
-hdiutil detach /Volumes/OS\ X\ Base\ System/
-rm /tmp/BaseSystem.dmg
-
-hdiutil convert /tmp/HighSierra.cdr.dmg -format UDTO -o /tmp/HighSierra.iso
-mv /tmp/HighSierra.iso.cdr ~/Desktop/HighSierra.iso
-rm /tmp/HighSierra.cdr.dmg
+hdiutil create -o /tmp/Sequoia -size 17g -volname ISO -layout SPUD -fs HFS+J -type UDTO -attach
+sudo /Applications/Install\ macOS\ Sequoia.app/Contents/Resources/createinstallmedia --volume /Volumes/ISO
+hdiutil detach /Volumes/ISO
+hdiutil convert /tmp/Sequoia.cdr -format UDTO -o ~/Desktop/Sequoia.iso
+rm -fv /tmp/Sequoia.cdr 
+mv ~/Desktop/Sequoia.iso.cdr ~/Desktop/Sequoia.iso
